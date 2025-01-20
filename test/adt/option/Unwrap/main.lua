9c8a3db34a24d941f67c3f8db984ad34aa1172ce
Yordan Mitev <yordanmitev@yordanmitev.me>
2025-01-21 00:01:49 +0100

    fixed broken test for option type

--- test/adt/option/Unwrap/main.lua
+++ test/adt/option/Unwrap/main.lua
@@ -3,9 +3,11 @@ package.path = os.getenv("SRC_DIR") .. '/?.lua;' .. package.path
 require("laskall")
 
 function Main()
-  local x = None();
-  local err = pcall(function () x:Unwrap() end)
-  if not err then
+  local x = Some(10);
+  local noerr, val = pcall(function () return x:Unwrap() end)
+  if noerr then
+    print(val)
+  else
     print("true")
   end
 end
